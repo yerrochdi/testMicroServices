d871f585afdf0a920e878415c11b155a75882f27
yerrochdi <yassine.errochdi@telecom-paristech.fr>
2018-10-15 16:39:25 +0200

    test

---
+++ services/client/src/index.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';  // new
+import UsersList from './components/UsersList';
+
+
+class App extends Component {
+  // new
+  constructor() {
+    super();
+    // new
+    this.state = {
+      users: []
+    };
+  };
+  // new
+  componentDidMount() {
+    this.getUsers();
+  };
+
+  getUsers() {
+    axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)  // new
+    .then((res) => { this.setState({ users: res.data.data.users }); })
+    .catch((err) => { console.log(err); });
+  };
+  render() {
+    return (
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-one-third">
+              <br/>
+              <h1 className="title is-1">All Users</h1>
+              <hr/><br/>
+              <UsersList users={this.state.users}/>
+            </div>
+          </div>
+        </div>
+      </section>
+    )
+  }
+};
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -3,19 +3,24 @@
 
 import os
 
-from flask import Flask  # new
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_debugtoolbar import DebugToolbarExtension  # new
+from flask_cors import CORS
 
 
-# instantiate the db
+# instantiate the extensions
 db = SQLAlchemy()
+toolbar = DebugToolbarExtension()  # new
 
 
-# new
 def create_app(script_info=None):
 
     # instantiate the app
     app = Flask(__name__)
+
+    # enable CORS
+    CORS(app)  # new
 
     # set config
     app_settings = os.getenv('APP_SETTINGS')
@@ -23,6 +28,7 @@ def create_app(script_info=None):
 
     # set up extensions
     db.init_app(app)
+    toolbar.init_app(app)
 
     # register blueprints
     from project.api.users import users_blueprint
