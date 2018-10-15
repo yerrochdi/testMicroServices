aea4d33ff763ffa3af8357d05b117810a12f5658
yerrochdi <yassine.errochdi@telecom-paristech.fr>
2018-10-15 14:26:42 +0200

    part2

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,19 +1,36 @@
 # services/users/project/__init__.py
 
 
-from flask import Flask, jsonify
+import os
+
+from flask import Flask  # new
+from flask_sqlalchemy import SQLAlchemy
 
 
-# instantiate the app
-app = Flask(__name__)
-
-# set config
-app.config.from_object('project.config.DevelopmentConfig')  # new
+# instantiate the db
+db = SQLAlchemy()
 
 
-@app.route('/users/ping', methods=['GET'])
-def ping_pong():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong!'
-    })
+# new
+def create_app(script_info=None):
+
+    # instantiate the app
+    app = Flask(__name__)
+
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+
+    # set up extensions
+    db.init_app(app)
+
+    # register blueprints
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
+
+    return app
--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,21 +1,27 @@
 # services/users/project/config.py
 
 
+import os  # new
+
+
 class BaseConfig:
     """Base configuration"""
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # new
+    SECRET_KEY = 'my_precious'
 
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # new
 
 
 class TestingConfig(BaseConfig):
     """Testing configuration"""
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')  # new
 
 
 class ProductionConfig(BaseConfig):
     """Production configuration"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # new
