4607400e80dced21aa7e267da45c7820979a61e4
yerrochdi <yassine.errochdi@telecom-paristech.fr>
2018-10-17 11:09:43 +0200

    second part

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -2,16 +2,20 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';  // new
 import UsersList from './components/UsersList';
+import AddUser from './components/AddUser';
 
 
 class App extends Component {
   // new
   constructor() {
     super();
-    // new
     this.state = {
-      users: []
+    users: [],
+    username: '',
+    email: '',
     };
+    this.addUser = this.addUser.bind(this);  // new
+    this.handleChange = this.handleChange.bind(this);
   };
   // new
   componentDidMount() {
@@ -28,10 +32,17 @@ class App extends Component {
       <section className="section">
         <div className="container">
           <div className="columns">
-            <div className="column is-one-third">
+            <div className="column is-half">  {/* new */}
               <br/>
               <h1 className="title is-1">All Users</h1>
               <hr/><br/>
+              <br/><br/>  {/* new */}
+              <AddUser
+                username={this.state.username}
+                email={this.state.email}
+                addUser={this.addUser}
+                handleChange={this.handleChange}  // new
+              />
               <UsersList users={this.state.users}/>
             </div>
           </div>
@@ -39,7 +50,26 @@ class App extends Component {
       </section>
     )
   }
+  addUser(event) {
+    event.preventDefault();
+    const data = {
+      username: this.state.username,
+      email: this.state.email
+    };
+    axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+    .then((res) => {
+      this.getUsers();  // new
+      this.setState({ username: '', email: '' });  // new
+    })
+    .catch((err) => { console.log(err); });
+  };
+  handleChange(event) {
+    const obj = {};
+    obj[event.target.name] = event.target.value;
+    this.setState(obj);
+  };
 };
+
 
 ReactDOM.render(
   <App />,
